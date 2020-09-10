47c8e1ecacd36d563b1dcbf530bf7ad473235f55
Seth <switfoth@gmail.com>
2020-09-10 15:34:24 -0500

    added logic for current player

--- connect-four.js
+++ connect-four.js
@@ -2,12 +2,20 @@ import { Game } from './game.js';
 let game = undefined;
 const boardHolder = document.getElementById('board-holder')
 const gameName = document.getElementById('game-name');
+const clickTargets = document.getElementById('click-targets')
 function updateUI() {
     if (game === undefined) {
         boardHolder.classList.add('is-invisible');
     } else {
         boardHolder.classList.remove('is-invisible');
-        gameName.innerHTML = getName();
+        gameName.innerHTML = game.getName();
+        if (game.currentPlayer === 1){
+            clickTargets.classList.remove("red")
+            clickTargets.classList.add("black")
+        } else {
+            clickTargets.classList.remove("black")
+            clickTargets.classList.add("red")
+        }
     }
 }
 
@@ -17,12 +25,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const p2Name = document.getElementById('player-2-name');
     const newGame = document.getElementById('new-game');
 
-
+    clickTargets.addEventListener('click', ()=>{
+        game.playInColumn();
+        updateUI();
+    })
 
     formHolder.addEventListener('keyup', (e) => {
-            if ((p1Name.value.length > 0) && (p2Name.value.length > 0)) {
-                    newGame.removeAttribute("disabled")
-            }
+        if ((p1Name.value.length > 0) && (p2Name.value.length > 0)) {
+            newGame.removeAttribute("disabled")
+        }
     })
 
     newGame.addEventListener('click', (e) => {
@@ -31,5 +42,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
         p2Name.value = '';
         newGame.setAttribute('disabled', 'true');
         updateUI();
+        console.log(game)
     })
 });
--- game.js
+++ game.js
@@ -1,9 +1,17 @@
-export class Game {
+ export class Game {
     constructor(player1, player2){
         this.playerOneName = player1;
         this.playerTwoName = player2;
+        this.currentPlayer = 1;
     }
     getName() {
         return `${this.playerOneName} vs. ${this.playerTwoName}`
+    }
+    playInColumn() {
+        if (this.currentPlayer === 1){
+            this.currentPlayer = 2
+        } else {
+            this.currentPlayer = 1
+        }
     }
 }
