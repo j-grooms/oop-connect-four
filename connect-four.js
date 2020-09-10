83d59938af3fc85e59dfd0e415c8ff87293b60af
Jacob Grooms <grooms.jacob@gmail.com>
2020-09-10 13:39:16 -0400

    first commit

---
+++ connect-four.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    let game = undefined;
+    const p1Name = document.getElementById('player-1-name');
+    const p2Name = document.getElementById('player-2-name');
+
+    p1Name.addEventListener('keyup', (e) => {
+        if ((p1Name.value.length > 0) && (p2Name.value.length > 0)) {
+            
+        }
+    })
+    p2Name.addEventListener('keyup', (e) => {
+        if ((p1Name.value.length > 0) && (p2Name.value.length > 0))
+    })
+});
